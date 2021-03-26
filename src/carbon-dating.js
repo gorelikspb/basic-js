c8693056d3bcf1b3e62efd108f590e721c23a769
Your <you@example.com>
2021-03-26 15:35:41 +0300

    init: basic js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,11 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (15<sampleActivity|| sampleActivity<=0 || !(typeof sampleActivity == 'string')){return false;}
+  // throw new CustomError('Not implemented');
+  // // remove line with error and write your code here
+
+  d = (Math.log2(MODERN_ACTIVITY/sampleActivity)*HALF_LIFE_PERIOD);
+  return Math.ceil(d) || false;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  arr = [];
+  if (!Array.isArray(members)) return false
+  for (let member of members) {
+    if (typeof member == 'string'){
+      arr.push(member.trim().charAt(0).toUpperCase());
+    }
+  }
+
+return arr.sort().join('');
+
 };
