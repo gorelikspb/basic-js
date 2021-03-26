81435a9b961850a611f637450136dba91d0e70fb
Your <you@example.com>
2021-03-27 00:06:49 +0300

    feat: hanoi & seasons

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  answer = {};
+  turns = 2**disksNumber - 1;
+  tps = turnsSpeed/3600;
+  answer['turns'] = turns;
+  answer['seconds'] = Math.floor(turns/tps);
+  return answer;
+
 };
