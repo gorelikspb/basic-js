11607c2a4fdcf4b82bd5f54fb21bc683b4e44713
Your <you@example.com>
2021-03-29 10:57:19 +0300

    feat: transform array (not all tests)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  arr = array.slice(0);
+  for (let i = 0; i<arr.length; i++){
+    if (arr[i]==undefined){
+      //do nothing
+    }
+    else if (arr[i] == '--double-next'){
+      arr[i]=arr[i+1];
+    }
+    else if  (arr[i] == '--double-prev'){
+      if (arr[i-1]){
+        arr[i] = arr[i-1];
+      }
+    }
+    else if  (arr[i] == '--discard-prev'){
+      arr[i-1]=undefined;
+    }
+    else if  (arr[i] == '--discard-next'){
+      arr[i+1]=undefined;
+    }
+  }
+
+  clear = []
+  for (let el of arr){
+    if (typeof el == 'number'){
+      clear.push(el);
+    }
+  }
+  
+  return clear;
 };
+
