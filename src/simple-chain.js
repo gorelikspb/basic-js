4c8e2093582c7535ab953d7e890b239d3f463aad
Your <you@example.com>
2021-03-29 12:20:01 +0300

    feat: simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  // chain: '',
+  chain: [],
   getLength() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // if (this.chain){
+    //   this.chain+='~~'
+    // }
+    // this.chain = this.chain +  '(' +value+')';
+    this.chain.push(value);
+    console.log(this.chain);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.chain[position]){
+      throw new CustomError('wrong position');
+    }
+    
+    this.chain.splice(position-1,1);
+    console.log(this.chain);
+    return this;
+
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // throw new CustomError('Not implemented');
+    // // remove line with error and write your code here
+
+    return '( ' + this.chain.join(' )~~( ') + ' )';
   }
 };
+
 
 module.exports = chainMaker;
